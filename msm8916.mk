b6a9cfff83f316f992f8e49a3293cbc62cca3833
DJABhipHop <djabhiphop-DJABHipHop@yahoo.com>
2022-03-13 13:15:50 -0400

    Test

--- msm8916.mk
+++ msm8916.mk
@@ -42,7 +42,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@5.0 \
     android.hardware.audio.effect@5.0-impl \
     android.hardware.soundtrigger@2.2-impl \
-    android.hardware.bluetooth.a2dp@1.0-impl \
     audio.a2dp.default \
     audio.bluetooth.default \
     audio.primary.msm8916 \
@@ -77,6 +76,7 @@ endif
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.0-impl \
+    android.hardware.bluetooth.a2dp@1.0-impl \
 
 PRODUCT_PACKAGES += \
     libbase_shim \
